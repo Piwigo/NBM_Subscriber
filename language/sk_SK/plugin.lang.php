73d3fc25e8a66d8107b7e8f33d8ac194dca4e413
dodo <dodo@piwigo.org>
2011-03-04 17:40:19 +0000

    git-svn-id: http://piwigo.org/svn/extensions/NBM_Subscriber@9533 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/sk_SK/plugin.lang.php
@@ -0,0 +1,19 @@
+<?php
+$lang['NBMS_Section'] = 'Upozornenie cez mail';
+$lang['NBMS_Text'] = 'Súhlas s upozornením cez mail';
+$lang['Yes'] = 'Áno';
+$lang['No'] = 'Nie';
+$lang['NBMS_Title'] = 'NBM člen';
+$lang['NBMS_Infos'] = 'Čo je to?';
+$lang['NBMS_Infos_Text'] = 'Piwigo ponúka dva druhy upozornení na zmeny v galérii:<br>
+- RSS upozornenie, použiteľné pre všetkých návštevníkov (dokonca aj pre neregistrovaných) v "Upozorňovacom" menu<br>
+- Upozornenie cez mail (NBM), použiteľné len pre administrátora alebo webmastra, ktorý je podobný informačnému bulletinu
+<br><br>
+Prednoťou tohto doplnku je ponúknuť každému <b><u>registrovanému</u></b> používateľovi zapísať sa alebo odhlásiť sa z NBM kedy len chcú. Na to je potrebné aktivovať doplnok, ktorý uvidíte ako voľbu na profilovej stránke.
+<br><br>
+<b>Dôležité</b>: Ak sa návštevník zapíše, alebo odpíše z NBM, nedostane potvrdzovací mail.
+<br><br>
+Poznámka: Tento doplnok je perfektný ;-)';
+$lang['NBMS_Support'] = 'Podpora tohto doplnku je len na Piwigo fóre:<br>
+<a href="http://piwigo.org/forum/viewtopic.php?id=16020" onclick="window.open(this.href);return false;">English forum - http://piwigo.org/forum/viewtopic.php?id=16020</a>';
+?>
