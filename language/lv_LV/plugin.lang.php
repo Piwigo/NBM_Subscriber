889d225da17188224b1b8c4d689782ebc4473c47
ddtddt <ddtddt@piwigo.org>
2011-04-16 05:22:07 +0000

    [extensions] - NBM_Subscriber - add lv_LV (Latvian) thanks to Aivars Baldone
    git-svn-id: http://piwigo.org/svn/extensions/NBM_Subscriber@10396 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/lv_LV/plugin.lang.php
@@ -0,0 +1,19 @@
+<?php
+$lang['NBMS_Section'] = 'Paziņojums pa pastu';
+$lang['NBMS_Text'] = 'Pieteikties paziņojumam pa pastu';
+$lang['Yes'] = 'Jā';
+$lang['No'] = 'Nē';
+$lang['NBMS_Title'] = 'NBM Parakstītājs';
+$lang['NBMS_Infos'] = 'Kas tas ir?';
+$lang['NBMS_Infos_Text'] = 'Piwigo piedāvā divus veidus kā paziņot par izmaiņām galerijā:<br>
+- Paziņojums ar RSS, pieejams visiem lietotājiem (pat nereģistrētiem) "Notification" (paziņojumu) izvēlnē<br>
+- Pziņojums pa pastu (NBM), pieejams atikai administratoriem vai vebmasteriem, kas ir  līdzīgs jaunumiem
+<br><br>
+Šī spraudņa mērķis ir piedāvāt ikvienam <b><u>reģistrētam</u></b> apmeklētājam pašam pieteikties vai atteikties no NBM pēc paša izvēles. Lai to paveiktu, vienkārši aktivējiet spraudni (ja esat izlasījis šo ziņu, tas nozīmē, ka to jau esat paveicis), un ieraudzīsiet jaunu opciju apmeklētāja profilā.
+<br><br>
+<b>Svarīgi</b> : Ja apmeklētāj, izmantojot šo, piesakās vai atsakās no NBM , no galerijas netiks nosūtīts e-pasta apstiprinājums, kā to darba administrators, izmantojot administratora paneli. Šī darbība ir paredzēta, lai izvairītos no dažu apmeklētāju negatīvas uzvedības, kas varētu novest pie no galerijas sūtītā e-pasta pārslodzes.
+<br><br>
+Piezīme: Šis spraudnis neatbalsta apmeklētāju informēšanu par šīs jaunās opcijas pieejamību. Jums, Administratoram / Vebmasteram, jāveic informācijas izvietošanu mēdijos. Šim nolūkam atbilstošs ir spraudnis PWG_Stuffs (PWG_Produkti);-)';
+$lang['NBMS_Support'] = 'Atbalsts un info par šo spraudni pieejama tikai Piwigo forumā:<br>
+<a href="http://piwigo.org/forum/viewtopic.php?id=16020" onclick="window.open(this.href);return false;">Forums angļu valodā - http://piwigo.org/forum/viewtopic.php?id=16020</a>';
+?>
