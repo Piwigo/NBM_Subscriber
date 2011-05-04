77cd5a3d70f3395074e46d24fd7f6b198a41fbcf
riopwg <riopwg@piwigo.org>
2011-05-04 19:02:40 +0000

    Update language IT
    git-svn-id: http://piwigo.org/svn/extensions/NBM_Subscriber@10764 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/it_IT/plugin.lang.php
+++ language/it_IT/plugin.lang.php
@@ -1,19 +1,19 @@
 <?php
-$lang['NBMS_Section'] = 'Notifica via e-mail';
-$lang['NBMS_Text'] = 'Iscriviti alla notifica via e-mail';
+$lang['NBMS_Section'] = 'Notifica via email';
+$lang['NBMS_Text'] = 'Iscriviti alla notifica via email';
 $lang['Yes'] = 'Sì';
 $lang['No'] = 'No';
 $lang['NBMS_Title'] = 'NBM Subscriber';
 $lang['NBMS_Infos'] = 'Che cosa è?';
-/* TODO */$lang['NBMS_Infos_Text'] = 'Piwigo offers two modes of notification of changes in the gallery:<br>
-- The RSS notification, usable by all visitors (even if not registered) in "Notification" menu<br>
-- Notification by mail (NBM), usable only by an administrator or webmaster that is similar to a newsletter
+$lang['NBMS_Infos_Text'] = 'Piwigo offre 2 metodi standard di notifica di cambiamenti avvenuti sulla galleria :<br>
+- La notifica via i flussi RSS, accessibili a tutti gli utenti (anche i non iscritti) via il menu "Notifiche"<br>
+- La notifica via email (NBM), accessibile solo a un amministratore o al webmaster, che può prendere la forma di una newsletter
 <br><br>
-The goal of this plugin is to offer to every <b><u>registered</u></b> visitor to subscribe or unsubscribe themselves from the NBM when he wants. To do this, simply activate the plugin (if you read this message, it means it\'s already done) to see a new option appear in the visitor\'s profile page.
+L\'obiettivo di questo plugin è di permettere a ogni utente <b><u>registrato</u></b> di potersi iscrivere o annullare l\'iscrizione da NBM quando lo desidera. Per fare ciò, basta attivare il plugin (se leggete questo messaggio, ciò vole dire che è già attivo) per vedrere apparire una nuova opzione nella pagina di personalizzazione degli utenti.
 <br><br>
-<b>Important</b> : If a visitor subscribes or unsubscribes from the NBM through it, no email confirmation will be sent from the gallery as is the case when an administrator does this through administration panel. This operation is intended to avoid negative behaviors of some visitors that would lead to a saturation of emails sending from the gallery.
+<b>Importante</b> : Se un utente si abbono o cancella l\'abbonamento via questo metodo, nessun email sarà mandato esattamente come se fosse stato un amministratore ad avere effettuato questa operazione via il pannello di amministrazione. Questo tipo di funzionamento e voluto per evitare problemi con l\'invio delle email (blocco dalla parte del provaider per abbuso di servizzio).
 <br><br>
-Note : This plugin does not support the visitor information on the availability of this new option. You, Administrator / Webmaster, have to implement an information medium attached. For this, the plugin PWG_Stuffs wil be perfect ;-)';
-/* TODO */$lang['NBMS_Support'] = 'The support on this plugin is only on this Piwigo\'s forum thread:<br>
-<a href="http://piwigo.org/forum/viewtopic.php?id=16020" onclick="window.open(this.href);return false;">English forum - http://piwigo.org/forum/viewtopic.php?id=16020</a>';
+Nota : Questo plugin non prende in carico l\'informazione agli utenti sulla messa a disposizione di questa nuova opzione. Tocca all\'amministratore / Webmaster ad informarne gli utenti. Per ciò, il plugin PWG_Stuffs è utilissimo ;-)';
+$lang['NBMS_Support'] = 'L\'assistenza per questo plugin si fà unicamente sul forum di Piwigo :<br>
+<a href="http://piwigo.org/forum/viewtopic.php?id=16020" onclick="window.open(this.href);return false;">Forum Inglese - http://piwigo.org/forum/viewtopic.php?id=16020</a>';
 ?>
