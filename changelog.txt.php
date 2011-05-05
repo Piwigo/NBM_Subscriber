243ee806b5a481daf709a2b91be081d4c8a440ca
Eric <Eric@piwigo.org>
2011-05-05 00:15:02 +0000

    Version 2.2.2 hard coded for publication (IT translation updated - Thx to Rio)
    git-svn-id: http://piwigo.org/svn/extensions/NBM_Subscriber@10767 68402e56-0260-453c-a942-63ccdbb3a9ee

--- changelog.txt.php
+++ changelog.txt.php
@@ -10,6 +10,15 @@ Plugin Name: NBM Subscriber
 -- 1.0.1 : Multilanguage bug fixed
 -- 1.0.2 : Bug fixed on adding new users in NBM list
 -- 1.0.3 : Localisation files update
--- 1.0.4 : Add of sk_SK translation - Thx to dodo ;-)
+-- 1.0.4 : Add of sk_SK translation - Thx to dodo
+
+
+**************************************
+***** Plugin history (branch 2.2)*****
+**************************************
+
+-- 2.2.0 : Compliance with Piwigo 2.2
+-- 2.2.1 : Add of lv_LV translation (thanks to Aivars Baldone)
+-- 2.2.2 : IT translation updated (thx to Rio)
 */
 ?>
