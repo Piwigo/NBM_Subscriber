280fe1e08df3c6408608d22bbebf780038c6608e
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-05-27 21:05:52 +0000

    [nbm_subscriber] Update cs_CZ, thanks to : lanius
    git-svn-id: http://piwigo.org/svn/extensions/NBM_Subscriber@15357 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/cs_CZ/plugin.lang.php
@@ -0,0 +1,40 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['NBMS_Infos_Text'] = 'Piwigo nabízí dva módy oznámení změn v galerii:<br>
+- RSS oznámení, použitelné pro všechny návštěvníky (dokonce i pro neregistrované) v "Notification" menu<br>
+- Oznámení mailem (NBM), použitelné pouze pro administrátora nebo webmastera, které je podobné informačnímu zpravodaji
+<br><br>
+Cílem tohoto pluginu je nabídnout každému <b><u>registrovanému</u></b> návštěvníkovi kdykolv se samostatně přihlásit nebo odhlásit z NBM. K tomu jednoduše aktivujte plugin (pokud čtete tento vzkaz, tak to znamená, že jste to udělal) a na stránce profilu návštěvníka se objeví nová možnost.
+<br><br>
+<b>Důležité</b> : Pokud se návštěvník přihlásí nebo odhlásí z NBM pomocí tohoto pluginu, nebude poslán žádný potvrzovací e-mail z galerie, stejně jako v případě, kdy administrátor toto udělá pomocí administračního panelu. Tento postup je zvolen k zabránění negativního chování některých návštěvníků, které může vést k zahlcení maily posílaných z galerie.
+<br><br>
+Poznámka : Tento plugin nepodporuje informování návštěvníků o dostupnosti této nové možnosti. Vy, adminstrátor / Webmaster, musíte zrealizovat přiložený informační prostředek. Pro tento účel bude plugin PWG_Stuffs perfektní ;-)';
+$lang['NBMS_Infos'] = 'Co to je?';
+$lang['NBMS_Section'] = 'Oznámení mailem';
+$lang['NBMS_Support'] = 'Podpora tohoto pluginu jr pouze na v tomto vláknu Piwigo fóra:<br>
+<a href="http://piwigo.org/forum/viewtopic.php?id=16020" onclick="window.open(this.href);return false;">Anglické fórum - http://piwigo.org/forum/viewtopic.php?id=16020</a>';
+$lang['NBMS_Text'] = 'Přihlásit se k oznámení mailem';
+$lang['NBMS_Title'] = 'NMB předplatitel';
+$lang['No'] = 'Ne';
+$lang['Yes'] = 'Ano';
+?>
