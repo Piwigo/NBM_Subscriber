88894dbe6e2c69fdb5b8a8591b8ef10235bc9d33
Eric <Eric@piwigo.org>
2012-06-16 10:55:54 +0000

    New version 2.4.1 hard coded for publication: Update uk_UA, thanks to : animan, rukhov and quest_tom Update cs_CZ, thanks to : lanius
    git-svn-id: http://piwigo.org/svn/extensions/NBM_Subscriber@15753 68402e56-0260-453c-a942-63ccdbb3a9ee

--- changelog.txt.php
+++ changelog.txt.php
@@ -5,10 +5,13 @@ Plugin Name: NBM Subscriber
 **************************************
 ***** Plugin history (branch 1.0)*****
 **************************************
-
+2010-06-01:
 -- 1.0.0 : Initial beta release for Piwigo compatibility
+2010-08-08:
 -- 1.0.1 : Multilanguage bug fixed
+2010-08-31:
 -- 1.0.2 : Bug fixed on adding new users in NBM list
+2011-03-04:
 -- 1.0.3 : Localisation files update
 -- 1.0.4 : Add of sk_SK translation - Thx to dodo
 
@@ -16,22 +19,29 @@ Plugin Name: NBM Subscriber
 **************************************
 ***** Plugin history (branch 2.2)*****
 **************************************
-
+2011-03-26:
 -- 2.2.0 : Compliance with Piwigo 2.2
+2011-04-16:
 -- 2.2.1 : Add of lv_LV translation (thanks to Aivars Baldone)
+2011-05-05:
 -- 2.2.2 : IT translation updated (thx to Rio)
 
 
 **************************************
 ***** Plugin history (branch 2.3)*****
 **************************************
+2011-10-01:
 -- 2.3.0 : Compliance with Piwigo 2.3
            DE translation updated (thx to Unphysicalix)
 
 **************************************
 ***** Plugin history (branch 2.4)*****
 **************************************
+2012-04-19:
 -- 2.4.0 : Compliance with Piwigo 2.4
            Add ru_RU translation (thanks to marchelly)
+2012-06-16:
+-- 2.4.1 : Update uk_UA, thanks to : animan, rukhov and quest_tom
+           Update cs_CZ, thanks to : lanius 
 */
 ?>
