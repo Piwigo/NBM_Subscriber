3a7edb04a6fccd18cc2e29946873a24d1f330005
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-07-15 13:56:41 +0000

    [nbm_subscriber] Update el_GR, thanks to : bas_alba
    git-svn-id: http://piwigo.org/svn/extensions/NBM_Subscriber@16750 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/el_GR/plugin.lang.php
@@ -0,0 +1,38 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['NBMS_Infos_Text'] = 'Το Piwigo προσφέρει δύο τρόπους για την κοινοποίηση των αλλαγών στη γκαλερί:<br>
+- Η κοινοποίηση RSS, χρήσιμο για όλους τους επισκέπτες (Ακόμα κι αν δεν έχουν καταχωριθεί) στο μενού "Γνωστοποίηση" <br>
+- Κοινοποίηση μέσω email (NBM), χρήσιμο μόνο για το διαχειριστή ή τον webmaster που είναι παρόμοιο με ένα ενημερωτικό δελτίο<br><br>
+Ο στόχος αυτού του πρόσθετου είναι να προσφέρει σε κάθε <u>καταχωρημένο</u></b> επισκέπτη να εγγραφεί ή να διαγραφεί από τις NBM όταν θέλει. Για να το κάνετε αυτό, ενεργοποιήστε απλά το πρόσθετο (αν διαβάσετε αυτό το μήνυμα, σημαίνει ότι το έχετε ήδη κάνει)και δείτε μια νέα επιλογή να εμφανίζετε στη σελίδα του προφίλ του επισκέπτη.<br><br>
+Εάν κάποιος επισκέπτης γίνει συνδρομητής ή διαγράφετε από συνδρομητής στο NBM μέσα από αυτό, χωρίς email επιβεβαίωσης να στέλνετε από τη συλλογή, όπως είναι στη περίπτωση που ένας διαχειριστής το κάνει μέσω του πίνακα διαχείρισης. Αυτή η λειτουργία έχει σκοπό να αποφευχθούν οι αρνητικές συμπεριφορές κάποιων επισκεπτών που θα οδηγήσει σε κορεσμό της αποστολής μηνυμάτων ηλεκτρονικού ταχυδρομείου από τη γκαλερί.
+<br><br>
+Σημείωση: Αυτό το πρόσθετο δεν υποστηρίζει πληροφορίες επισκεπτών όσον αφορά την διαθεσιμότητα της νέας αυτής δυνατότητας. Εσείς ως Διαχειριστής / Webmaster, πρέπει να εφαρμόσετε ένα μέσο για τις πληροφορίες που επισυνάπτονται. Για το σκοπό αυτό, το πρόσθετο PWG_Stuffs είναι το τέλειο ;-)';
+$lang['NBMS_Infos'] = 'Τι είναι αυτό;';
+$lang['NBMS_Section'] = 'Κοινοποίηση μέσω ταχυδρομείου';
+$lang['NBMS_Support'] = 'Η Υποστήρηξη για αυτό το πρόσθετο είναι μόνο μέσω ενός φόρουμ του Piwigo:<br>
+<a href="http://piwigo.org/forum/viewtopic.php?id=16020" onclick="window.open(this.href);return false;">Αγγλικό φόρουμ - http://piwigo.org/forum/viewtopic.php?id=16020</a>';
+$lang['NBMS_Text'] = 'Εγγραφή για κοινοποίηση μέσω ταχυδρομείου';
+$lang['NBMS_Title'] = 'Συνδρομητής NBM ';
+$lang['No'] = 'Όχι';
+$lang['Yes'] = 'Ναι';
+?>
