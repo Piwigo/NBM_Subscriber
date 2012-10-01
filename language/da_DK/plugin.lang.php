f901fd46462c288a76c2aab3dd2c118f5f6703bd
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-10-01 21:16:10 +0000

    [nbm_subscriber] Update da_DK, thanks to : Kaare
    git-svn-id: http://piwigo.org/svn/extensions/NBM_Subscriber@18414 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/da_DK/plugin.lang.php
@@ -0,0 +1,40 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['NBMS_Infos_Text'] = 'Piwigo tilbyder to metoder til notifikation vedrørende ændringer i galleriet:<br>
+- RSS-notifikation, brugbar for alle besøgende (selv hvis de ikke er registeret) i "Notifikation"-menu<br>
+- Notifikation pr. mail (NBM), kun brugbar for en administrator eller webmaster, svarende til et nyhedsbrev
+<br><br>
+Formålet med plugin\'en er at tilbyde alle <b><u>registrerede</u></b> besøgende, at tilmelde eller afmelde sig selv til NBM, når de ønsker det. Man skal blot aktivere plugin\'en (hvis du læser denne besked, er det allerede gjort) for at en ny valgmulighed dukker op på de besøgendes profilsider.
+<br><br>
+<b>Vigtigt</b>: Hvis en besøgende tilmelder eller afmelder sig fra NBM derigennem, vil der ikke blive sendt en bekræftelse pr. mail fra galleriet, som det er tilfældet når en administrator gør det gennem administrationspanel. Det er for at undgå nogle brugeres uønskede handlinger, som kan føre til at mange mails sendes fra galleriet.
+<br><br>
+Bemærk: Plugin\'en understøtter ikke, at brugerne får besked om tilgængeligheden af denne funktion. Du, administrator/webmaster, skal selv implementere en oplysningsmetode. Til det formål er plugin\'en PWG_Stuffs perfekt. ;-)';
+$lang['NBMS_Infos'] = 'Hvad er det?';
+$lang['NBMS_Section'] = 'Notifikation pr. mail';
+$lang['NBMS_Support'] = 'Support af denne plugin er kun tilgængelig i denne Piwigo-forumtråd:<br>
+<a href="http://piwigo.org/forum/viewtopic.php?id=16020" onclick="window.open(this.href);return false;">engelsk forum - http://piwigo.org/forum/viewtopic.php?id=16020</a>';
+$lang['NBMS_Text'] = 'Tilmeld notifkation pr. mail';
+$lang['NBMS_Title'] = 'NBM-tilmeldt';
+$lang['No'] = 'Nej';
+$lang['Yes'] = 'Ja';
+?>
