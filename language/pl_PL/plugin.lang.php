c1dbb16d8f20682725edcc0fe0b8896701085e75
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-02-16 06:57:00 +0000

    [nbm_subscriber] Update pl_PL, thanks to : K.S.
    git-svn-id: http://piwigo.org/svn/extensions/NBM_Subscriber@20771 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/pl_PL/plugin.lang.php
+++ language/pl_PL/plugin.lang.php
@@ -24,4 +24,6 @@ $lang['NBMS_Section'] = 'Powiadomienia przez e-mail';
 $lang['No'] = 'Nie';
 $lang['Yes'] = 'Tak';
 $lang['NBMS_Infos'] = 'Co to jest?';
+$lang['NBMS_Support'] = 'Wsparcie dla tej wtyczki jest udzielane wyłącznie w tym wątku na forum Piwigo:<br />
+<a href="http://piwigo.org/forum/viewtopic.php?id=16020" onclick="window.open(this.href);return false;">Forum anglojęzyczne - http://piwigo.org/forum/viewtopic.php?id=16020</a>';
 ?>
