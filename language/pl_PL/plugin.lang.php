c611d5a24a151a54ecaafe78643c07f6f0b586dd
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-03-06 16:02:09 +0000

    [nbm_subscriber] Update pl_PL, thanks to : K.S.
    git-svn-id: http://piwigo.org/svn/extensions/NBM_Subscriber@21246 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/pl_PL/plugin.lang.php
+++ language/pl_PL/plugin.lang.php
@@ -27,4 +27,14 @@ $lang['NBMS_Infos'] = 'Co to jest?';
 $lang['NBMS_Support'] = 'Wsparcie dla tej wtyczki jest udzielane wyłącznie w tym wątku na forum Piwigo:<br />
 <a href="http://piwigo.org/forum/viewtopic.php?id=16020" onclick="window.open(this.href);return false;">Forum anglojęzyczne - http://piwigo.org/forum/viewtopic.php?id=16020</a>';
 $lang['NBMS_Text'] = 'Subskrybuj powiadomienia na mail';
+$lang['NBMS_Infos_Text'] = 'Piwigo oferuje dwa tryby powiadamiania o zmianach w galerii:<br />
+- Powiadomienia RSS, nadające się do użytku przez wszystkich odwiedzających (nawet niezarejestrowanych) w menu "Powiadomienia".<br />
+- Powiadomienia na e-mail (NBM), przeznaczone wyłącznie dla administratora i webmastera, działają podobnie do biuletynu (newlettera).
+<br /><br />
+Wtyczka ta powstała w celu zaoferowania każdemu <b><u>zarejestrowanemu</u></b> użytkownikowi możliwości zapisania i wypisania się w dowolnym momencie z powiadomień NBM. Aby to umożliwić po prostu uaktywnij ją (jeżeli czytasz tę wiadomość tzn., że już to zrobiłeś), a nowe opcje pojawią się w profilu odwiedzającego.
+<br /><br />
+<b>Ważne</b>: Jeżeli użytkownik zapisuje lub wypisuje się z NBM to żadne potwierdzenie na e-mail nie zostanie wysłanie jak ma to miejsce w przypadku, gdy robi to administrator poprzez swój panel. Ma to na celu zapobiec niepożądanym zachowaniom niektórych użytkowników, które mogłyby prowadzić do zwiększenia ilości maili wysyłanych z galerii.
+<br /><br />
+Uwaga: This plugin does not support the visitor information on the availability of this new option. You, Administrator / Webmaster, have to implement an information medium attached. Do tego celu idealna będzie wtyczka PWG_Stuffs ;)';
+$lang['NBMS_Title'] = 'Subskrybent NBM';
 ?>
