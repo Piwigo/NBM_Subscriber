2b278ec81825d354bd01ea74073723b0f418b6ae
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-04-09 13:19:37 +0000

    [nbm_subscriber] Add pt_BR, thanks to : flaviove
    git-svn-id: http://piwigo.org/svn/extensions/NBM_Subscriber@22084 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/pt_BR/plugin.lang.php
@@ -0,0 +1,40 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['NBMS_Infos'] = 'O que é isto?';
+$lang['NBMS_Infos_Text'] = 'Piwigo oferece dois modos de notificação de alterações na galeria:<br>
+- A notificação de RSS, utilizável por todos os visitantes (mesmo que não registrado) em menu "Notificação"<br>
+- A notificação por e-mail (NBM), utilizável apenas por um administrador ou webmaster que é semelhante a um boletim
+<br><br>
+O objetivo deste plugin é oferecer a cada <b><u>registrado</u></b> visitante  se cadastrar ou descadastrar-se da NBM quando quiser. Para fazer isso, basta ativar o plugin (se você ler esta mensagem, significa que ele já fez) para ver uma nova opção aparecerá na página do visitante perfil.
+<br><br>
+<b>Importante</b>:Se um visitante subscreve ou unsubscribes da NBM através dele, nnenhuma confirmação de e-mail será enviada a partir da galeria como é o caso quando um administrador faz isso através do painel de administração. Esta operação destina-se a evitar comportamentos negativos de alguns visitantes que levaria a uma saturação de enviar e-mails a partir da galeria.
+<br><br>
+Nota: Este plugin não oferece suporte a visitante sobre informações da disponibilidade desta nova opção. Você, Administrador/Webmaster, tem que implementar um meio de informação em anexo. Para isso, os plugins PWG_Stuffs wil ser perfeito;-)';
+$lang['NBMS_Section'] = 'Notificação por e-mail';
+$lang['NBMS_Support'] = 'O suporte para este plugin é só neste segmento do fórum Piwigo:<br>
+<a href="http://piwigo.org/forum/viewtopic.php?id=16020" onclick="window.open(this.href);return false;">Fórum em Inglês - http://piwigo.org/forum/viewtopic.php?id=16020</a>';
+$lang['NBMS_Text'] = 'Assinar notificação por email';
+$lang['NBMS_Title'] = 'Assinante NBM';
+$lang['No'] = 'Não';
+$lang['Yes'] = 'Sim';
+?>
