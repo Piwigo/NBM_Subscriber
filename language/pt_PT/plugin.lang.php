0f5a89f964346f14ce3a3dbd0072744b719c794c
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-05-19 20:18:20 +0000

    [nbm_subscriber] Update pt_PT, thanks to : ANO
    git-svn-id: http://piwigo.org/svn/extensions/NBM_Subscriber@22743 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/pt_PT/plugin.lang.php
@@ -0,0 +1,38 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['NBMS_Infos'] = 'O que é isso?';
+$lang['Yes'] = 'Sim';
+$lang['No'] = 'Não';
+$lang['NBMS_Title'] = 'Subscrever NBM ';
+$lang['NBMS_Text'] = 'Subscrição de notificação por mail.';
+$lang['NBMS_Support'] = 'Suporte para esta extensão apenas existe o fórum Piwigo:<br>
+<a href="http://piwigo.org/forum/viewtopic.php?id=16020" onclick="window.open(this.href);return false;">English forum - http://piwigo.org/forum/viewtopic.php?id=16020</a>';
+$lang['NBMS_Section'] = 'Notificação por mail';
+$lang['NBMS_Infos_Text'] = 'Piwigo oferece dois modos de notificação de alterações na galeria: <br>
+- Notificação RSS, utilizável por todos os visitantes (mesmo que não registados) no menu "Notificação"<br>
+- Notificação por mail (NBM), utilizável apenas por um administrador ou webmaster; é semelhante a uma circular informativa
+<br>
+O objetivo desta extensão é oferecer a cada visitante <b><u> registado</u></b> subscrever ou anular subscrição de Notificação por Mail (NBM) quando desejar. Para tal, basta ativar a extensão(se você ler esta mensagem, significa que já procedeu) para ver uma nova opção aparecer na página de perfil de visitante.<br>
+<b>Importante</b>: Se um visitante subscrever ou anular subscrição da NBM através da extensão, não será enviado email de confirmação  a partir da galeria tal como faz um administrador através do painel de administração. Esta operação visa evitar comportamentos negativos de alguns visitantes que levariam a uma saturação de envio de e-mails enviados a partir da galeria.<br><br>
+Nota: Esta extensão não suporta a informação ao visitante acerca da disponibilidade desta nova opção. Você, Administrador / Webmaster, deve implementar, em anexo, a informação respetiva. Para isso, a extensão PWG_Stuffs será adequada;-)';
+?>
