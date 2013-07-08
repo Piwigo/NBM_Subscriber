d95301e1c554c7911165284c29305f63380a7594
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-07-08 19:10:06 +0000

    [nbm_subscriber] Add nl_NL, thanks to : Kees Hessels
    git-svn-id: http://piwigo.org/svn/extensions/NBM_Subscriber@23880 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/nl_NL/plugin.lang.php
@@ -0,0 +1,38 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['NBMS_Infos_Text'] = 'Piwigo biedt twee manieren van kennisgeving bij wijzigingen in de galerie: <br>
+- De RSS kennisgeving, bruikbaar door alle bezoekers (ook indien niet geregistreerd) in het "Kennisgeving" menu <br>
+- Kennisgeving per mail (Notification By Mail, NBM), alleen bruikbaar door een beheerder of webmaster, vergelijkbaar met een nieuwsbrief
+<br><br>
+Het doel van deze plugin is om het voor elke <b><u>geregistreerde</u></b> bezoeker mogelijk te maken zich aan of af te melden voor NBM wanneer deze dat wilt. Om dit te doen activeert u de plugin (als u dit bericht leest, betekent dit dat u dat al hebt gedaan) zodat een nieuwe optie verschijnt op de bezoekers-profielpagina
+<br><br>
+<b>Belangrijk</b>: Als een bezoeker zich hiermee aan- of afmeld voor NBM, wordt geen bevestigingsmail verzonden vanaf de galerie zoals dat wel het geval is indien een beheerder doet dit door middel van het administratiescherm. Deze "operatie" is bedoeld om negatief gedrag van sommige bezoekers, dat zou leiden tot een veelvuldig e-mail-verkeer vanuit de galerie, te voorkomen.
+<br><br>
+Opmerking: Deze plugin maakt de bezoeker niet attent op de beschikbaarheid van deze nieuwe optie. U, de administrator / webmaster, zult hier zelf iets voor moeten regelen. Hiervoor kunt u bijvoorbeeld gebruik maken van de plugin PWG_Stuffs ;-)';
+$lang['NBMS_Support'] = 'Support voor deze plugin alleen via het Piwigo forum:<br>
+<a href="http://piwigo.org/forum/viewtopic.php?id=16020" onclick="window.open(this.href);return false;">Engels forum - http://piwigo.org/forum/viewtopic.php?id=16020</a>';
+$lang['NBMS_Infos'] = 'Wat is het?';
+$lang['NBMS_Section'] = 'Kennisgeving per mail';
+$lang['NBMS_Text'] = 'Abonneer u op de kennisgeving per post';
+$lang['NBMS_Title'] = 'NMB Abonnee';
+?>
