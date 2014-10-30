81c836e14a19a2e18246c4d278e4943c6c1a31ce
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-10-30 12:33:20 +0000

    [nbm_subscriber] Update nb_NO, thanks to : paulen
    git-svn-id: http://piwigo.org/svn/extensions/NBM_Subscriber@30334 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/nb_NO/plugin.lang.php
@@ -0,0 +1,36 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['NBMS_Infos'] = 'Hva er det?';
+$lang['NBMS_Infos_Text'] = 'Piwigo tilbyr to moduser av melding om endringer i galleriet:<br>
+- RSS-varsling, kan brukes av alle besøkende (selv om en ikke er registrert) i "Varslings" -menyen<br>
+- Varsling via e-post (NBM), kan kun brukes av en administrator eller webmaster. Denne  ligner på et nyhetsbrev
+<br><br>
+Målet med dette programtillegget er å tilby alle <b><u> registrerte </u></b> besøkende til å abonnere på NBM når han ønsker. For å gjøre dette, er det bare å aktivere programtillegget (hvis du leser denne meldingen, betyr det at det allerede er gjort) for å se et nytt alternativ vise i profilen på den besøkendes side.
+<br><br>
+<B>Viktig</b>: Hvis en besøkende abonnerer eller stopper abonnemenet fra NBM, vil ingen e-post bekreftelse bli sendt fra galleriet som er tilfellet når en administrator gjør dette gjennom administrasjonspanel. Denne operasjonen er ment for å unngå negativ atferd fra noen besøkende som igjen ville føre til en opphoping av e-poster sendt fra galleriet.
+<br><br>
+Merk: Dette tillegget støtter ikke den besøkendes informasjon om tilgjengeligheten av dette nye alternativet. Du, Administrator / Webmaster, må gjennomføre et informasjonsmedium vedlagt. For dette, vil programtillegget PWG_Stuffs være perfekt;-)';
+$lang['NBMS_Section'] = 'Varsling via e-post';
+$lang['NBMS_Support'] = 'Støtten til dette programtillegget finnes kun på Piwigo\'s forum tråder:<br><a href="http://piwigo.org/forum/viewtopic.php?id=16020" onclick="window.open(this.href);return false;">English forum - http://piwigo.org/forum/viewtopic.php?id=16020</a>';
+$lang['NBMS_Text'] = 'Abboner på meldinger via e-post';
+$lang['NBMS_Title'] = 'NBM Abonnement';
